0044a22f62c92dcf5f8875a387efddbe11a6caec
Jon Janzen <jonjanzen@me.com>
2013-12-13 16:40:48 -0700

    changed vimrc.after to vimrc, added dircolors

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ cwd=$(pwd)
 ln -s $cwd/bash_aliases $HOME/.bash_aliases
 ln -s $cwd/bashrc $HOME/.bashrc
 ln -s $cwd/gitconfig $HOME/.gitconfig
-ln -s $cwd/vimrc.after $HOME/.vimrc.after
+ln -s $cwd/vimrc $HOME/.vimrc
 ln -s $cwd/zprofile $HOME/.zprofile
 ln -s $cwd/zshrc $HOME/.zshrc
+ln -s $cwd/dircolors $HOME/.dircolors
---
+++ vimrc
@@ -0,0 +1,8 @@
+syntax enable
+colorscheme koehler
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+
